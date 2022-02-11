6592150a086f1f62c6d3fd94f7d34b535c480467
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-02-11 21:10:36 +0300

    added Room model

--- main/views.py
+++ main/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id':1, 'name':"Lets learn python"},
-    {'id':2, 'name':"Design with me"},
-    {'id':2, 'name':"Backend developers"},
-]
+from .models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'main/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'main/room.html', context)
