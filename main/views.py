dea03536a8bf75410f6449977764f74412c332df
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-02-15 22:44:11 +0300

    added search form

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -9,7 +10,10 @@ def home(request):
     else:
         q = ''
 
-    rooms = Room.objects.filter(topic__name__contains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q)|
+                                Q(name__icontains=q)|
+                                Q(description__icontains=q)
+                                )
 
     topics = Topic.objects.all()
 
