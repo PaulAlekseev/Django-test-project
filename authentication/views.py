1f04be0c5a943db7d4c137d8d3c2ff51f3bcec4c
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-02-16 22:05:03 +0300

    user login done

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        else:
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Username OR password does not exist')
+
+    context = {}
+    return render(request, 'authentication/login_register.html', context)
