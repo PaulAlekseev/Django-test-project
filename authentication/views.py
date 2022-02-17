f8c2398f98bd47acef14d5bc70603dac5251f5f3
Павел Алексеев <pavel.zommerfeld@yandex.ru>
2022-02-17 21:14:02 +0300

    restricted edit,delete buttons and login page

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
 
     if request.method == "POST":
         username = request.POST.get('username')
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
@@ -28,6 +30,7 @@ def room(request, pk):
     return render(request, 'main/room.html', context)
 
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -38,9 +41,14 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'main/room_form.html', context)
 
+
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse("Your not allowed here")
 
     if request.method == "POST":
         form = RoomForm(request.POST, instance=room)
@@ -52,8 +60,12 @@ def updateRoom(request, pk):
     return render(request, 'main/room_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse("Your not allowed here")
 
     if request.method == 'POST':
         room.delete()
